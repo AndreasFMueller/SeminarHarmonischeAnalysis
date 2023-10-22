cdda26a40160359296ae197bb0117a8e67ce23bf
Andreas Müller <andreas.mueller@ost.ch>
2023-10-22 11:53:32 +0200

    review chapter 1

--- cover/spec.h
+++ cover/spec.h
@@ -9,6 +9,7 @@
 #include "wav.h"
 #include <fftw3.h>
 #include <cmath>
+#include <cstdio>
 
 namespace wavreader {
 
@@ -18,7 +19,8 @@ typedef std::shared_ptr<spec_base>	spec_ptr;
 
 class spec_base {
 public:
-	virtual double	operator()(int i) const = 0;
+	virtual double	operator()(unsigned int i) const = 0;
+	virtual double	f(unsigned int i) const = 0;
 	static spec_ptr	get(chunk_ptr cp, int size, int initialoffset,
 		int channels, int channel);
 	virtual ~spec_base() { }
@@ -31,8 +33,12 @@ template<int n>
 class spec : public spec_base {
 	double	data[n];
 	double	spectrum[n];
+	int	spectrumsize = n/2 + 1;
+	double	values[n/2 + 1];
+	double	lb;
 public:
 	spec(chunk_ptr cp, int initialoffset, int channels, int channel) {
+		lb = log2(n/2);
 		// read sound data
 		for (int i = 0; i < n; i++) {
 			frame f = cp->getFrame(initialoffset + i, channels);
@@ -43,21 +49,49 @@ public:
 		// perform spectral analysis
 		{
 			double	in[n];
-			memcpy(in, data, sizeof(double) * n);
 			fftw_plan	p = fftw_plan_r2r_1d(n, in, spectrum,
 						FFTW_R2HC, FFTW_MEASURE);
+			memcpy(in, data, sizeof(double) * n);
 			fftw_execute(p);
 			fftw_destroy_plan(p);
 			//std::cout << "FFT complete" << std::endl;
 		}
+
+		// compute values
+		values[0] = fabs(spectrum[0]);
+		for (unsigned int i = 0; i < n / 2; i++) {
+			values[i] = hypot(spectrum[i], spectrum[n-i]);
+		}
+		values[n/2] = fabs(spectrum[n / 2 + 1]);
 	}
 	virtual ~spec() { }
 
-	double	operator()(int i) const {
+	double	operator()(unsigned int i) const {
+		return values[i];
+	}
+
+	static double	expindex(unsigned int i) {
+		return (n/2) * exp(5 * ((i / (double)(n/2 + 1)) - 1));
+	}
+
+	double	f(unsigned int i) const {
 		if (i == 0) {
-			return spectrum[0];
+			return values[0];
 		}
-		return hypot(spectrum[i], spectrum[n-i]);
+		//int	j = floor((n/2) * log2(i) / lb);
+		double	t = expindex(i);
+		int	j = floor(t);
+//printf("%d,%d\n", i, j);
+		if (t >= n/2+1) {
+printf("%d,%f\n", i, t);
+		//	throw std::runtime_error("j exceeds bounds");
+		}
+		if (t < 0) {
+			return 0;
+		} 
+		double	T = t - floor(t);
+		
+		return (1-T) * values[j] + T * values[j+1];
 	}
 };
 
--- cover/wavreader.cpp
+++ cover/wavreader.cpp
@@ -19,8 +19,6 @@ namespace wavreader {
 
 int	wav_debug = 0;
 
-#define N 2048
-
 static void	usage(const char *progname) {
 	std::cout << "usage:" << std::endl << std::endl;
 	std::cout << "    " << progname;
@@ -28,16 +26,101 @@ static void	usage(const char *progname) {
 	std::cout << std::endl << std::endl;
 	std::cout << "options:" << std::endl;
 	std::cout << "  -d,--debug             run in debug mode" << std::endl;
+	std::cout << "  -n,--length            block length for FFT" << std::endl;
 	std::cout << "  -h,-?,--help           display some help" << std::endl;
 	std::cout << "  -f,--fits=image.fits   write image to file image.fits";
+	std::cout << "  -l,--limit=<lim>       limit samples to this value"
+		<< std::endl;
+	std::cout << "  -L,--log               take logarithm values" << std::endl;
+	std::cout << "  -o,--octaves           logarithmically map the frequency scale" << std::endl;
+	std::cout << "  -s,--step=<s>          use steps of size s instead of N" << std::endl;
 	std::cout << std::endl;
 }
 
 static struct option	options[] = {
+{ "color",		no_argument,			NULL,	'c' },
 { "debug",		no_argument,			NULL,	'd' },
 { "fits",		required_argument,		NULL,	'f' },
 { "help",		no_argument,			NULL,	'h' },
+{ "limit",		required_argument,		NULL,	'l' },
+{ "log",		no_argument,			NULL,	'L' },
+{ "length",		required_argument,		NULL,	'n' },
+{ "octaves",		no_argument,			NULL,	'o' },
+{ "step",		required_argument,		NULL,	's' },
 { NULL,			0,				NULL,	 0  }
+};
+
+class color {
+public:
+	float	rgb[3];
+	color() {
+		rgb[0] = rgb[1] = rgb[2] = 0;
+	}
+	color(float r, float g, float b) {
+		rgb[0] = r;
+		rgb[1] = g;
+		rgb[2] = b;
+	}
+	color(const color& other) {
+		for (int i = 0; i < 3; i++) {
+			rgb[i] = other.rgb[i];
+		}
+	}
+	color	operator*(float t) {
+		return color(t * rgb[0], t * rgb[1], t * rgb[2]);
+	}
+	color	operator+(const color& other) {
+		return color(	rgb[0] + other.rgb[0],
+				rgb[1] + other.rgb[1],
+				rgb[2] + other.rgb[2]);
+	}
+	float	R() const { return rgb[0]; }
+	float	G() const { return rgb[1]; }
+	float	B() const { return rgb[2]; }
+};
+
+typedef struct { float top; color farbe; } colorpoint;
+
+class colormap {
+	static int	n;
+	static colorpoint	points[6];
+public:
+	static color	get(double value) {
+		if (value <= points[0].top) {
+			return points[0].farbe;
+		}
+		for (int i = 0; i < n; i++) {
+			if ((value > points[i].top)
+				&& (value <= points[i+1].top)) {
+				float	d = points[i+1].top - points[i].top;
+				float	t = (value - points[i].top) / d;
+				return (points[i].farbe * (1-t))
+					+ (points[i+1].farbe * t);
+			}
+		}
+		if (value > points[n-1].top) {
+			return points[n-1].farbe;
+		}
+		throw std::runtime_error("color not found");
+	}
+};
+
+int 	colormap::n = 6;
+colorpoint	colormap::points[6] = {
+#if 0
+	{ 0.0, color(0.0, 0.0, 0.8) },
+	{ 0.1, color(0.6, 0.0, 0.6) },
+	{ 0.2, color(0.8, 0.0, 0.8) },
+	{ 0.3, color(1.0, 0.0, 0.2) },
+	{ 0.4, color(1.0, 0.8, 0.0) },
+	{ 1.0, color(1.0, 1.0, 1.0) }
+#endif
+	{ 0.0, color(0.0, 0.0, 1.0) },
+	{ 0.1, color(0.6, 0.2, 1.0) },
+	{ 0.2, color(0.8, 0.0, 0.4) },
+	{ 0.3, color(0.8, 0.0, 0.0) },
+	{ 0.4, color(1.0, 0.2, 0.0) },
+	{ 1.0, color(1.0, 0.8, 0.0) }
 };
 
 /**
@@ -46,11 +129,17 @@ static struct option	options[] = {
 int	main(int argc, char *argv[]) {
 	std::string	fitsfilename;
 	std::string	wavfilename;
+	double	limit = 0;
+	int	N = 8192;
+	int	stepsize = 512;
+	bool	logarithmic = false;
+	bool	octaves = false;
+	bool	use_color = false;
 
 	// parse the command line
 	int	c;
 	int	longindex;
-	while (EOF != (c = getopt_long(argc, argv, "f:dh?",
+	while (EOF != (c = getopt_long(argc, argv, "cf:dh?l:Los:",
 			options, &longindex)))
 		switch (c) {
 		case 'f':
@@ -63,6 +152,24 @@ int	main(int argc, char *argv[]) {
 		case '?':
 			usage(argv[0]);
 			return EXIT_SUCCESS;
+			break;
+		case 'l':
+			limit = std::stod(optarg);
+			break;
+		case 'L':
+			logarithmic = true;
+			break;
+		case 'n':
+			N = std::stoi(optarg);
+			break;
+		case 's':
+			stepsize = std::stoi(optarg);
+			break;
+		case 'o':
+			octaves = true;
+			break;
+		case 'c':
+			use_color = true;
 			break;
 		}
 	
@@ -78,17 +185,16 @@ int	main(int argc, char *argv[]) {
 	chunk_ptr	cp = w.current();
 
 	// create a FITS data structure
-	double	limit = 10000;
 	int	blocksize = N;
-	int	stepsize = N / 4;
 	int	spectrumsize = blocksize / 2 + 1;
 	std::cout << "spectrum size: " << spectrumsize << std::endl;
 	int	nblocks = cp->length() / (4 * stepsize);
 	std::cout << "blocks: " << nblocks << std::endl;
 	long	fpixel = 1;
-	long	naxis = 2;
-	long	naxes[2] = { nblocks, spectrumsize };
-	long	nelements = naxes[0] * naxes[1];
+	long	naxis = (use_color) ? 3 : 2;
+	long	naxes[3] = { nblocks, spectrumsize, 3 };
+	long	pixels = nblocks * spectrumsize;
+	long	nelements = pixels * ((use_color) ? 3 : 1);
 	std::cout << "creating buffer with " << nelements << " elements";
 	std::cout << std::endl;
 	float	*imagedata = new float[nelements];
@@ -102,16 +208,38 @@ int	main(int argc, char *argv[]) {
 	fits_report_error(stderr, status);
 
 	std::cout << "writing image data" << std::endl;
-	for (int block = 0; block < nblocks; block++) {
+	for (int block = 0; block < nblocks - 100; block++) {
 		int	offset = block * stepsize;
-		spec<N>	s(cp, offset, 2, 0);
+		spec_ptr	s = spec_base::get(cp, N, offset, 2, 0);
 		// process the spectrum
+		double	v;
 		for (int i = 0; i < spectrumsize; i++) {
-			double	v = fabs(s(i));
+			if (octaves) {
+				v = fabs(s->f(i));
+			} else {
+				v = fabs(s->operator()(i));
+			}
 			//std::cout << v << std::endl;
 			//std::cout << block * spectrumsize + i << std::endl;
-			imagedata[block + nblocks * i]
-				= (v > limit) ? limit : v;
+			if (limit > 0) {
+				v = (v > limit) ? limit : v;
+			}
+			if (logarithmic) {
+				if (v > 1) {
+					v = log(v);
+				} else {
+					v = 0;
+				}
+			}
+			if (use_color) {
+				color	c = colormap::get(v / limit);
+				int	offset = block + nblocks * i;
+				imagedata[offset] = c.R();
+				imagedata[offset + pixels] = c.G();
+				imagedata[offset + 2 * pixels] = c.B();
+			} else {
+				imagedata[block + nblocks * i] = v;
+			}
 		}
 		offset += stepsize;
 	}
